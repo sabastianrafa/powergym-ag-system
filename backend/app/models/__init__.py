7e6d1cc67848b8107202819bcf702f975ae072de
Santiago Afanador <afanadorgaleano@gmail.com>
2025-10-01 15:42:49 -0500

    initialize models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,31 @@
+from .customer import (
+    CustomerBase,
+    CustomerCreate,
+    CustomerUpdate,
+    CustomerResponse,
+    DocumentType,
+    GenderType,
+    RecordStatus
+)
+from .biometric import (
+    BiometricBase,
+    BiometricCreate,
+    BiometricUpdate,
+    BiometricResponse,
+    BiometricType
+)
+
+__all__ = [
+    "CustomerBase",
+    "CustomerCreate",
+    "CustomerUpdate",
+    "CustomerResponse",
+    "DocumentType",
+    "GenderType",
+    "RecordStatus",
+    "BiometricBase",
+    "BiometricCreate",
+    "BiometricUpdate",
+    "BiometricResponse",
+    "BiometricType",
+]
